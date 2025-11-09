b7f8d01e4b046e6b313ab4cdd92d99512a7bb030
Marek Forys <mforys73@gmail.com>
2025-11-09 01:02:37 +0100

    feat: Splash screen
    Signed-off-by: Marek Forys <mforys73@gmail.com>

--- PokerGame/PokerGameApp.swift
+++ PokerGame/PokerGameApp.swift
@@ -2,9 +2,24 @@ import SwiftUI
 
 @main
 struct PokerGameApp: App {
+    @State private var isShowingLaunch = true
+    
     var body: some Scene {
         WindowGroup {
-            GameView()
+            if isShowingLaunch {
+                LaunchScreenView()
+                    .onAppear {
+                        // Show launch screen for 2 seconds then transition to main game
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                            withAnimation(.easeInOut(duration: 0.5)) {
+                                isShowingLaunch = false
+                            }
+                        }
+                    }
+            } else {
+                GameView()
+                    .transition(.opacity)
+            }
         }
     }
 }
---
+++ PokerGame/Views/LaunchScreenView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        ZStack {
+            Color.green
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 20) {
+                Image(systemName: "suit.spade.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.white)
+                
+                Image(systemName: "suit.heart.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.red)
+                
+                Image(systemName: "suit.diamond.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.red)
+                
+                Image(systemName: "suit.club.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.white)
+                
+                Text("Poker Game")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.top, 20)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
